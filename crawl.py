0cedb6cfde79671b4b67adee9fd34775e807923f
Kristian Stöckel <git@k118.de>
2021-07-28 16:19:38 +0200

    Python3 compatibility

--- crawl.py
+++ crawl.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import requests
-import sys
-import re
-import json
-import lxml.html as lh
-import os.path
-import os
+
 import hashlib
+import sys
+import urllib.parse as urlparse
+from pathlib import Path
+from urllib.parse import parse_qs
+import lxml.html as lh
+import requests
+from bs4 import BeautifulSoup
 
 icms_username = '***-***-u1'
 icms_password = '****'
@@ -16,7 +17,8 @@ telegram_chatID = '****'
 
 
 def telegram_bot_sendtext(bot_message):
-    send_text = 'https://api.telegram.org/bot' + telegram_bot_token + '/sendMessage?chat_id=' + telegram_chatID + '&parse_mode=Markdown&text=' + bot_message
+    send_text = 'https://api.telegram.org/bot' + telegram_bot_token + '/sendMessage?chat_id=' \
+                + telegram_chatID + '&parse_mode=Markdown&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
 
@@ -29,7 +31,7 @@ payload = {
 
 session_requests = requests.session()
 session_requests.headers.update({
-                                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'})
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'})
 
 login_url = "https://icms.hs-hannover.de/qisserver/rds?state=user&type=0"
 print("Rufe Startseite auf")
@@ -43,40 +45,27 @@ result = session_requests.post(
     data=payload
 )
 
-result = session_requests.get(
-    "https://icms.hs-hannover.de/qisserver/rds?state=sitemap&topitem=leer&breadCrumbSource=portal")
+# Extract SessionID
+asi = None
 
-try:
-    link_notenspiegel = re.search('<a href="(.+?)" class="regular">Notenspiegel</a>', result.content).group(1)
-    print("Link gefunden!")
-    link_notenspiegel = link_notenspiegel.replace("amp;", "")
-    print(link_notenspiegel)
-except AttributeError:
-    print("Error: Link Notenspiegel wurde nicht gefunden.")
-    sys.exit()
+soup = BeautifulSoup(str(result.content), 'html.parser')
+for link in soup.find_all('a'):
+    parsed = urlparse.urlparse(link.get('href'))
+    params = parse_qs(parsed.query)
+    if "asi" in params:
+        asi = params.get("asi")[0]
 
-print("Rufe Notenspiegel Seite auf...")
-result = session_requests.get(
-    link_notenspiegel,
-    headers=dict(referer="https://icms.hs-hannover.de/qisserver/rds?state=sitemap&topitem=leer&breadCrumbSource=portal")
-)
-
-try:
-    link_notenuebersicht = re.search('<a href="(.+?)" title="Leistungen für Abschluss', result.content).group(1)
-    print("Link gefunden!")
-    link_notenuebersicht = link_notenuebersicht.replace("amp;", "")
-    print(link_notenuebersicht)
-except AttributeError:
-    print("Error: Link Notenuebersicht wurde nicht gefunden.")
+if asi is None:
+    print("SessionID couldn't be extracted")
     sys.exit()
 
 print("Rufe Notenuebersicht Seite auf...")
 result = session_requests.get(
-    link_notenuebersicht,
+    "https://icms.hs-hannover.de/qisserver/rds?state=notenspiegelStudent&next=list.vm&nextdir=qispos/notenspiegel/student&createInfos=Y&struct=auswahlBaum&nodeID=auswahlBaum%7Cabschluss%3Aabschl%3D84%2Cstgnr%3D1&expand=0&asi=" + asi,
     headers=dict(referer="https://icms.hs-hannover.de/qisserver/rds?state=sitemap&topitem=leer&breadCrumbSource=portal")
 )
 
-doc = lh.fromstring(result.content)
+doc = lh.fromstring(str(result.content))
 table_elements = doc.xpath('//table')
 notenuebersicht_table = table_elements[1]
 
@@ -92,22 +81,25 @@ for tr in notenuebersicht_table:
     credits = 0
     semester = 0
     for td in tr:
+        text = str(td.text.replace("\\t", "").replace("\\r", "").replace("\\n", "").strip())
+
         i = i + 1
-        if (i == 1):
-            pruefungsnr = td.text.strip()
-        if (i == 2):
-            pruefungstext = td.text.strip()
-        if (i == 3):
-            art = td.text.strip()
-        if (i == 4):
-            note = td.text.strip()
-        if (i == 5):
-            status = td.text.strip()
-        if (i == 7):
-            credits = td.text.strip()
-        if (i == 9):
-            semester = td.text.strip()
-    if (art != "PL"):
+        if i == 1:
+            pruefungsnr = text
+        if i == 2:
+            pruefungstext = text
+        if i == 3:
+            art = text
+        if i == 4:
+            note = text
+        if i == 5:
+            status = text
+        if i == 7:
+            credits = text
+        if i == 9:
+            semester = text
+
+    if art != "PL":
         continue
 
     if semester not in noten:
@@ -116,16 +108,22 @@ for tr in notenuebersicht_table:
         'pruefungstext': pruefungstext,
         'note': note,
         'status': status,
-        'credits': credits
+        'credits': int(credits)
     }
 
 for semester in noten:
     for pruefungsnr in noten[semester]:
-        hash = hashlib.md5(semester + pruefungsnr + noten[semester][pruefungsnr]["status"]).hexdigest()
+        toHash = semester + pruefungsnr + noten[semester][pruefungsnr]["status"]
+        hash = hashlib.md5(toHash.encode("UTF-8")).hexdigest()
+
+        knownHashes = Path('examcheck.txt').read_text()
         f = open('examcheck.txt', 'a+')
-        if hash not in f.read():
+        if hash not in knownHashes:
             f.write(hash + "\n")
-            telegram_bot_sendtext(
-                "Neuer Pruefungsstatus \nModul: " + noten[semester][pruefungsnr]["pruefungstext"] + "\nStatus: " +
-                noten[semester][pruefungsnr]["status"] + "\nNote: " + noten[semester][pruefungsnr]["note"])
+
+            message = "Neuer Pruefungsstatus " \
+                      "\nModul: " + noten[semester][pruefungsnr]["pruefungstext"] + \
+                      "\nStatus: " + noten[semester][pruefungsnr]["status"] + \
+                      "\nNote: " + noten[semester][pruefungsnr]["note"]
+            telegram_bot_sendtext(message)
         f.close()
